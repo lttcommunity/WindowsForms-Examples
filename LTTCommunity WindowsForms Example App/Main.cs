f67dc1add6b8cc4655f911b4d46430b1d71ba7a4
AluminiumTech <aluminiumdev@gmail.com>
2017-04-10 12:19:26 +0800

    started work on windows forms example app

---
+++ LTTCommunity WindowsForms Example App/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LTTCommunity_WindowsForms_Example_App
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+           Text += ProductName + " " + ProductVersion;
+        }
+    }
+    }
